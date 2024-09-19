e560ede2ebf200ae9f1732033c13e534d3ff6913
Tane4ka170 <tane4ka170@gmail.com>
2024-09-19 23:46:35 +0300

    Added audio data array with song names and paths

---
+++ src/audioData.js
@@ -0,0 +1,22 @@
+export const audios = [
+  {
+    name: "Praying",
+    music: require("./audios/Kesha – Praying.mp3"),
+  },
+  {
+    name: "Confetti",
+    music: require("./audios/Little Mix - Confetti.mp3"),
+  },
+  {
+    name: "Wild Life",
+    music: require("./audios/OneRepublic - Wild Life.mp3"),
+  },
+  {
+    name: "You Da One",
+    music: require("./audios/Rihana – You Da One.mp3"),
+  },
+  {
+    name: "Only Want You",
+    music: require("./audios/Rita Ora - Only Want You.mp3"),
+  },
+];
