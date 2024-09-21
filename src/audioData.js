499336dddd90b67cd296c29b80acc3a7bb6bd6bd
Tane4ka170 <tane4ka170@gmail.com>
2024-09-21 23:13:07 +0300

    Added creator property to audio data

--- src/audioData.js
+++ src/audioData.js
@@ -2,21 +2,26 @@ export const audios = [
   {
     name: "Praying",
     music: require("./audios/Kesha – Praying.mp3"),
+    creator: "Kesha",
   },
   {
     name: "Confetti",
     music: require("./audios/Little Mix - Confetti.mp3"),
+    creator: "Little Mix",
   },
   {
     name: "Wild Life",
     music: require("./audios/OneRepublic - Wild Life.mp3"),
+    creator: "OneRepublic",
   },
   {
     name: "You Da One",
     music: require("./audios/Rihana – You Da One.mp3"),
+    creator: "Rihanna",
   },
   {
     name: "Only Want You",
     music: require("./audios/Rita Ora - Only Want You.mp3"),
+    creator: "Rita Ora",
   },
 ];
