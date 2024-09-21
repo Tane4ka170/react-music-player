49debb67545010c6c2a1ffde9233431a1ad43098
Tane4ka170 <tane4ka170@gmail.com>
2024-09-21 23:15:12 +0300

    Added play/pause functionality with state

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiPlay1, CiPause1 } from "react-icons/ci";
 import { RxTrackPrevious, RxTrackNext } from "react-icons/rx";
 
 export default function Player() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  };
   return (
     <div>
       <div className="player-card">
+        <h3 className="activeSong-name">Song name</h3>
+        <h5 className="activeArtist-name">Artist Name</h5>
         <div className="control-icon">
-          <RxTrackPrevious color="#278234" size={50} />
-          <CiPlay1 color="#278234" size={50} />
-          <CiPause1 color="#278234" size={50} />
-          <RxTrackNext color="#278234" size={50} />
+          <RxTrackPrevious color="black" size={50} className="icons" />
+          {isPlaying ? (
+            <CiPause1
+              color="#278234"
+              size={70}
+              className="icons"
+              onClick={togglePlay}
+            />
+          ) : (
+            <CiPlay1
+              color="#278234"
+              size={70}
+              className="icons"
+              onClick={togglePlay}
+            />
+          )}
+          <RxTrackNext color="black" size={50} className="icons" />
         </div>
       </div>
     </div>
