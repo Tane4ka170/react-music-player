90fc0b2b4da73339d5826263f75793639c32b773
Tane4ka170 <tane4ka170@gmail.com>
2024-09-21 21:32:06 +0300

    Added play/pause and skip controls

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { CiPlay1, CiPause1 } from "react-icons/ci";
+import { RxTrackPrevious, RxTrackNext } from "react-icons/rx";
 
 export default function Player() {
   return (
     <div>
-      <div className="player-card"></div>
+      <div className="player-card">
+        <div className="control-icon">
+          <RxTrackPrevious color="#278234" size={50} />
+          <CiPlay1 color="#278234" size={50} />
+          <CiPause1 color="#278234" size={50} />
+          <RxTrackNext color="#278234" size={50} />
+        </div>
+      </div>
     </div>
   );
 }
