1a4854311283152306de4d1d0584df8a5684eb97
Tane4ka170 <tane4ka170@gmail.com>
2024-09-22 23:09:17 +0300

    Added audio functionality to Player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { CiPlay1, CiPause1 } from "react-icons/ci";
 import { RxTrackPrevious, RxTrackNext } from "react-icons/rx";
 
-export default function Player() {
+export default function Player({ currentSong, currentIndex }) {
   const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+
   const togglePlay = () => {
     setIsPlaying(!isPlaying);
   };
+
+  useEffect(() => {}, []);
   return (
     <div>
+      <audio ref={audioRef} src={currentSong.music}></audio>
       <div className="player-card">
-        <h3 className="activeSong-name">Song name</h3>
-        <h5 className="activeArtist-name">Artist Name</h5>
+        {currentSong ? (
+          <div>
+            <h3 className="activeSong-name">{currentSong.name}</h3>
+            <h5 className="activeArtist-name">{currentSong.creator}</h5>
+          </div>
+        ) : (
+          ""
+        )}
+
         <div className="control-icon">
           <RxTrackPrevious color="black" size={50} className="icons" />
           {isPlaying ? (
