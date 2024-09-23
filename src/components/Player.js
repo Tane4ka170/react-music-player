f96d2945bedde1d52da1dfbd10b65381e273495f
Tane4ka170 <tane4ka170@gmail.com>
2024-09-23 17:45:08 +0300

    Implemented audio playback and controls

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,7 +2,12 @@ import React, { useEffect, useRef, useState } from "react";
 import { CiPlay1, CiPause1 } from "react-icons/ci";
 import { RxTrackPrevious, RxTrackNext } from "react-icons/rx";
 
-export default function Player({ currentSong, currentIndex }) {
+export default function Player({
+  currentSong,
+  currentIndex,
+  nextSong,
+  prevSong,
+}) {
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef(null);
 
@@ -10,22 +15,34 @@ export default function Player({ currentSong, currentIndex }) {
     setIsPlaying(!isPlaying);
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying, currentIndex]);
+
   return (
     <div>
       <audio ref={audioRef} src={currentSong.music}></audio>
       <div className="player-card">
         {currentSong ? (
           <div>
-            <h3 className="activeSong-name">{currentSong.name}</h3>
-            <h5 className="activeArtist-name">{currentSong.creator}</h5>
+            <h2 className="activeSong-name">{currentSong.name}</h2>
+            <h4 className="activeArtist-name">{currentSong.creator}</h4>
           </div>
         ) : (
           ""
         )}
 
         <div className="control-icon">
-          <RxTrackPrevious color="black" size={50} className="icons" />
+          <RxTrackPrevious
+            color="black"
+            size={50}
+            className="icons"
+            onClick={prevSong}
+          />
           {isPlaying ? (
             <CiPause1
               color="#278234"
@@ -41,7 +58,12 @@ export default function Player({ currentSong, currentIndex }) {
               onClick={togglePlay}
             />
           )}
-          <RxTrackNext color="black" size={50} className="icons" />
+          <RxTrackNext
+            color="black"
+            size={50}
+            className="icons"
+            onClick={nextSong}
+          />
         </div>
       </div>
     </div>
