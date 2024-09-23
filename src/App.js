c5d5bfb646871e8b67be1046fc0f75e00118e385
Tane4ka170 <tane4ka170@gmail.com>
2024-09-23 17:44:04 +0300

    Implemented song navigation

--- src/App.js
+++ src/App.js
@@ -6,17 +6,32 @@ import Player from "./components/Player";
 
 function App() {
   const [songs, setSongs] = useState(audios);
-  const [currentSong, setCurrentSong] = useState({});
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   const [currentIndex, setCurrentIndex] = useState(null);
   const getSongData = (song, index) => {
     setCurrentIndex(index);
     setCurrentSong(song);
   };
 
+  const nextSong = () => {
+    setCurrentIndex(currentIndex + 1);
+    setCurrentSong(audios[currentIndex + 1]);
+  };
+
+  const prevSong = () => {
+    setCurrentIndex(currentIndex - 1);
+    setCurrentSong(audios[currentIndex - 1]);
+  };
+
   return (
     <>
       <div className="player-main">
-        <Player currentSong={currentSong} currentIndex={currentIndex} />
+        <Player
+          currentSong={currentSong}
+          currentIndex={currentIndex}
+          nextSong={nextSong}
+          prevSong={prevSong}
+        />
       </div>
       <div className="app-main">
         {songs.map((song, index) => (
